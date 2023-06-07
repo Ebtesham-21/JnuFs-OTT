f475a0d6db1def08510064b17351d51249d7e57f
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-06-07 13:26:37 +0600

    prisma solved

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -21,11 +21,12 @@ const Auth = () => {
         <div className="relative h-full w-full bg-[url('/images/collage-1.png')]">
             <div className="bg-black w-full h-full bg-opacity-50">
                 <nav className="px-12 py-5">
-                    <img src="/images/logo.png" alt="logo" className="h-12" />
+                    {/* <img src="/images/logo.png" alt="logo" className="h-12" /> */}
 
                 </nav>
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md  w-full">
+                        <img src="/images/logo.png" alt="logo" className="h-16" />
                         <h2 className="text-white text-4xl mb-8 font-semibold">{variant == 'login' ? 'Sign in' : 'Register '}</h2>
                         <div className="flex flex-col gap-4">
                             {variant == 'register' && (     <Input
