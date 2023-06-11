a86903159e56c43e7f1f54634f8c3a62a24bd65b
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-06-11 23:30:05 +0600

    11.6.23 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
+import useCurrentUser from '@/hooks/useCurrentuser';
 import { NextPageContext } from 'next';
-import {signOut} from 'next-auth/react';
+import {getSession,signOut} from 'next-auth/react';
 
 export async function getServerSideProps(context:NextPageContext) {
   const session = await getSession(context);
@@ -19,6 +20,8 @@ export async function getServerSideProps(context:NextPageContext) {
 }
 
 export default function Home() {
+  const { } = useCurrentUser();
+
   return (
     <>
     <h1>NEtFLIX CLONE</h1>
