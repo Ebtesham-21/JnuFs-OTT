89588f8d3d3bf5e1e2796986dabf0a6431b0fac1
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-06-16 03:09:13 +0600

    prisma bug fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
 import Navbar from '@/components/Navbar';
 import useCurrentUser from '@/hooks/useCurrentuser';
+import useMovieList from '@/hooks/useMovieList';
 import { NextPageContext } from 'next';
 import {getSession,signOut} from 'next-auth/react';
 
@@ -22,6 +24,7 @@ export async function getServerSideProps(context:NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
  
 
 
@@ -29,6 +32,12 @@ export default function Home() {
     <>
       <Navbar />
       <Billboard/>
+    
+      <div className='pb-40'>
+       <MovieList title='Trending Now' data={movies} />
+      </div>
+      
+
    
     
     
