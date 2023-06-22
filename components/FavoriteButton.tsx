105bf5eed79e6f0d5857b2b83315b77ffdb06056
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-06-22 15:23:13 +0600

    play done without youtube intregration + download off kora lagbye

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -50,23 +50,24 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({
 
     return (
         <div
-         onClick={toggleFavorites}
-         className="cursor-pointer
-        group/item
-        w-6
-        h-6
-        lg:w-10
-        lg:h-10
-        border-white
-        border-2
-        rounded-full
-        flex
-        justify-center
-        items-center
-        transition
-        hover:border-neutral-300
-        ">
-            <Icon className="text-white" size={23}/>
+        //  onClick={toggleFavorites}
+        //  className="cursor-pointer
+        // group/item
+        // w-6
+        // h-6
+        // lg:w-10
+        // lg:h-10
+        // border-white
+        // border-2
+        // rounded-full
+        // flex
+        // justify-center
+        // items-center
+        // transition
+        // hover:border-neutral-300
+        // ">
+        //     <Icon className="text-white" size={23}/>
+        >
 
 
 
